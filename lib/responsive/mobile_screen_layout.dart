8a056b17b02c741285282a0d756cc0c2d9a9fe78
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-09-20 13:50:57 +0300

    Create mobile layout widget

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -5,6 +5,12 @@ class MobileScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'This is mobile',
+        ),
+      ),
+    );
   }
 }
