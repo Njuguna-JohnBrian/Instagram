c5e8114f48e4ae7a9ed5dd8f7ab4b1adc7cba2c4
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-09-20 13:51:06 +0300

    Create web layout widget

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -5,6 +5,12 @@ class WebScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'This is web',
+        ),
+      ),
+    );
   }
 }
