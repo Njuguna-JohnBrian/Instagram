c9a2d5610fb9b9da8477bc5c4bf7c6e65f459ee6
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-09-20 16:11:02 +0300

    Center login widget components

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32,
+          ),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // svg image
+              // email text field
+              // password text field
+              // login button
+              // signup transition
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
