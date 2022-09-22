19563d58142db29e3f9747628c3e2e0d0c436420
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-09-22 12:58:06 +0300

    Use modelled data

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instagram/resources/storage_methods.dart';
+import '../models/user.dart' as model;
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -33,15 +34,19 @@ class AuthMethods {
             .uploadImageToStorage('profilePics', file, false);
 
         // add user to database
-        await _firestore.collection('users').doc(cred.user?.uid).set({
-          'username': username,
-          'uid': cred.user?.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl
-        });
+        model.User user = model.User(
+          username: username,
+          uid: cred.user!.uid,
+          email: email,
+          photoUrl: photoUrl,
+          bio: bio,
+          followers: [],
+          following: [],
+        );
+        await _firestore
+            .collection('users')
+            .doc(cred.user?.uid)
+            .set(user.toJson());
 
         // await _firestore.collection('users').add({
         //   'username': username,
