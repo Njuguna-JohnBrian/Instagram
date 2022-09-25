d8a13684d4e7061d6e17386187875d8d90da54f8
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-09-25 10:58:59 +0300

    Render post card

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram/utils/colors.dart';
 
+import '../widgets/post_card.dart';
+
 class FeedScreen extends StatelessWidget {
   const FeedScreen({Key? key}) : super(key: key);
 
@@ -25,6 +27,7 @@ class FeedScreen extends StatelessWidget {
           )
         ],
       ),
+      body:const PostCard(),
     );
   }
 }
